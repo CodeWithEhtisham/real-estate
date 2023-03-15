ab5c874f2a49b1fbaaff202941280c8d5d4ad4a9
CodeWithEhtisham <shamikhan0347@gmail.com>
2023-03-15 12:29:37 +0500

    urls added

--- config/setting/base.py
+++ config/setting/base.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user',
+    'apps.admin_dashboard',
+    'apps.user_dashboard'
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    path('user/', include('apps.user_dashboard.urls')),
+    path('admin/', include('apps.admin_dashboard.urls')),
+    path('', include('user.urls')),
 ]
