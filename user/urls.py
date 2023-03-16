47c2cb02b2c545544851daf8b3f88d0579edecd4
CodeWithEhtisham <shamikhan0347@gmail.com>
2023-03-16 22:56:55 +0500

    user login page added

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from .views import login, logout
 urlpatterns = [
+    path('',login,name='login'),
+    # path('register',views.register,name='register'),
+    path('logout',logout,name='logout'),
 ]
