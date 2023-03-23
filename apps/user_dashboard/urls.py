3992cf0bc47f26ad899e8e0b978569e0b58d932c
CodeWithEhtisham <shamikhan0347@gmail.com>
2023-03-23 16:32:05 +0500

    user dashboard html code clean

--- apps/user_dashboard/urls.py
+++ apps/user_dashboard/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import user_dashboard
+from .views import user_dashboard, gallery, contact, properties_details, properties, blog_archive, blog_single
 
 urlpatterns = [
     path('', user_dashboard, name='index'),
+    path('gallery/', gallery, name='gallery'),
+    path('contact/', contact, name='contact'),
+    path('properties_details/', properties_details, name='properties_details'),
+    path('properties/', properties, name='properties'),
+    path('blog_archive/', blog_archive, name='blog_archive'),
+    path('blog_single/', blog_single, name='blog_single'),
 ]
--- apps/user_dashboard/views.py
+++ apps/user_dashboard/views.py
@@ -3,3 +3,21 @@ from django.shortcuts import render
 # Create your views here.
 def user_dashboard(request):
     return render(request, 'user_dashboard/index.html')
+
+def gallery(request):
+    return render(request, 'user_dashboard/gallery.html')
+
+def contact(request):
+    return render(request, 'user_dashboard/contact.html')
+
+def properties_details(request):
+    return render(request, 'user_dashboard/properties-detail.html')
+
+def properties(request):
+    return render(request, 'user_dashboard/properties.html')
+
+def blog_archive(request):
+    return render(request, 'user_dashboard/blog-archive.html')
+
+def blog_single(request):
+    return render(request, 'user_dashboard/blog-single.html')
